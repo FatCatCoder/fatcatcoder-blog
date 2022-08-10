7aefa5c75c543e9395e59cf8107d87bb77c957bb
FatCatCoder <christian75armand@gmail.com>
2022-08-09 23:44:17 -0400

    post

---
+++ components/bits/Quoteblock.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Quoteblock(props) {
+  return (
+    <div class="max-w-4xl p-4 text-gray-800 bg-white rounded-lg shadow">
+        <div class="mb-2">
+            <div class="h-3 text-3xl text-left text-gray-600">“</div>
+            <p class="px-4 text-sm text-center text-gray-600">
+                {props.body}
+            </p>
+            <div class="h-3 text-3xl text-right text-gray-600">”</div>
+        </div>
+    </div>
+  )
+}
